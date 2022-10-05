47936be73f09ab45695ba3f7d4d9266503f2bea1
Dauka03 <dauka0300@gmail.com>
2022-10-05 21:39:15 +0600

    update

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    password: "a5s03020302",
-    host: "localhost",
+    user: "ydjufuegoavqlx",
+    password: "431a6db7a2d98be06893ae48bf7ad1bc71438c2839edee5a1eb198238c24e216",
+    host: "ec2-44-205-63-142.compute-1.amazonaws.com",
     port: 5432,
-    database: "ata_db"
+    database: "d5vpkhrl13olul"
 })
 
 module.exports = pool
